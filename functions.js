56051df68f3c560adfd370e10ef817b3b738412f
hishambokhari <hish.b92@gmail.com>
2021-01-26 11:46:51 +0000

    Returning full name

--- functions.js
+++ functions.js
@@ -1,5 +1,6 @@
 const functions = {
   add: (num1,num2) => num1 + num2,
+  name: (first, last) => first + " " + last,
 }
 
 
--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,7 @@ const functions = require ('./functions');
 test('Add 2 + 2 to equal 4', () => {
   expect(functions.add(2,2)).toBe(4);
 });
+
+test('Add first name and last name', () => {
+  expect(functions.name("Hisham","Bokhari")).toBe("Hisham Bokhari");
+});
